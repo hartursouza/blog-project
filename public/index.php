3970bdf6b875b65fe07d5f3bef65eb59752f7041
Hartur Souza <hartursouza3@gmail.com>
2024-05-04 20:51:38 -0300

    Added initial configs, style.css and index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Project styles -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Project Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;1,300&display=swap"
+        rel="stylesheet">
+    <title>Blog</title>
+</head>
+
+<body>
+    <h1>Hello, World!</h1>
+</body>
+
+</html>
